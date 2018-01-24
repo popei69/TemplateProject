ffdfd6b0ae4925b36e216ce4194389e7435bf16a
Benoit PASQUIER <b.pasquier69@gmail.com>
2018-01-24 22:23:59 +0000

    Update code architecture

---
+++ TemplateProject/ViewModel/CurrencyDataSource.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyDataSource.swift
+//  TemplateProject
+//
+//  Created by Benoit PASQUIER on 24/01/2018.
+//  Copyright © 2018 Benoit PASQUIER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GenericDataSource<T> : NSObject {
+    var data: DynamicValue<[T]> = DynamicValue([])
+}
+
+class CurrencyDataSource : GenericDataSource<CurrencyRate>, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell", for: indexPath) as! CurrencyCell
+        
+        let currencyRate = self.data.value[indexPath.row]
+        cell.currencyRate = currencyRate
+        
+        return cell
+    }
+}
