6c7b47de707f1ca4742be7500ead6e744b5f2564
Benoit PASQUIER <b.pasquier69@gmail.com>
2019-02-06 12:04:53 +0800

    Fixing api and using local data instead for time being + updating unit test to match

--- TemplateProjectTests/CurrencyTests.swift
+++ TemplateProjectTests/CurrencyTests.swift
@@ -71,21 +71,3 @@ class CurrencyTests: XCTestCase {
         }
     }
 }
-
-extension FileManager {
-    
-    static func readJson(forResource fileName: String ) -> Data? {
-    
-        let bundle = Bundle(for: CurrencyTests.self)
-        if let path = bundle.path(forResource: fileName, ofType: "json") {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                return data
-            } catch {
-                // handle error
-            }
-        }
-        
-        return nil
-    }
-}
